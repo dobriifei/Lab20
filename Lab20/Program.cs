856066170494c1b1771583e4ba41838810208dd6
vermei2@outlook.com <vermei2@outlook.com>
2022-02-05 16:22:56 +0300

    Добавьте файлы проекта.

---
+++ Lab20/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab20
+{
+    class Program
+
+    {
+        delegate double MyDelegate(double R);
+        static void Main(string[] args)
+        {
+            MyDelegate myDelegate = Length;
+            myDelegate += Square;
+            myDelegate += Volume;
+            myDelegate(3.1);
+            Console.ReadKey();
+        }
+
+        static double Length(double R)
+        {
+            double D = Math.PI * (R) * 2;
+            Console.WriteLine(D);
+            return D;
+        }
+        static double Square(double R)
+        {
+            double S = Math.PI * (R) * R;
+            Console.WriteLine(S);
+            return S;
+        }
+        static double Volume(double R)
+        {
+            double V = 4 / 3 * Math.PI * R * R * R;
+            Console.WriteLine(V);
+            return V;
+        }
+    }
+}
